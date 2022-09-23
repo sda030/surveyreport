cd0b52cb067f465841276be524e6117ed7f82865
Stephan Daus <stephan.daus@gmail.com>
2022-09-24 00:13:55 +0200

    0.0.1.9003

--- R/observe_multiple_comb.R
+++ R/observe_multiple_comb.R
@@ -1,65 +1,72 @@
 
 #' Extention to the infer package for multiple testing
 #'
-#' @inheritParams infer::specify
-#' @inheritParams infer::hypothesize
-#' @inheritParams infer::calculate
+#' @inheritParams infer::t_test
+#'
 #' @importFrom dplyr mutate across
-#' @importFrom tidyselect eval_select
+#' @importFrom tidyselect eval_select all_of
 #' @importFrom tidyr drop_na
 #' @importFrom labelled remove_val_labels
 #' @importFrom broom tidy
 #' @importFrom purrr map_dfr
 #' @importFrom rlang set_names enquo
-#' @importFrom stats t.test
+#' @importFrom infer t_test
 #' @return A tibble containing for each response-explanatory row combination
 #' columns summarizing the test statistic as obtained from broom::tidy().
 #' @export
 #'
 #' @examples
-observe_multiple_comb <-
+#' library(dplyr)
+#' x <- t_test_multiple_comb(x = mtcars,
+#'                      response = matches("mpg|disp|drat|hp|qsec"),
+#'                      explanatory = matches("vs|am"))
+t_test_multiple_comb <-
   function(x,
-           # specify arguments
-
            formula = NULL,
            response = NULL,
            explanatory = NULL,
-           success = NULL,
-           null = NULL,
-           p = NULL,
-           mu = NULL,
-           med = NULL,
-           sigma = NULL,
-           stat=
-             c("mean", "median", "sum", "sd", "prop", "count", "diff in means",
-               "diff in medians", "diff in props", "Chisq", "F", "slope", "correlation", "t", "z",
-               "ratio of props", "odds ratio")
+           order = NULL,
+           alternative = "two-sided",
+           mu = 0,
+           conf_int = TRUE,
+           conf_level = 0.95
   ) {
 
-    response_cols <- tidyselect::eval_select(enquo(response), x)
-    explanatory_cols <- tidyselect::eval_select(enquo(explanatory), x)
-    x <-
-      tidyr::drop_na(x, response_cols, explanatory_cols)
+    response_cols <- tidyselect::eval_select(rlang::enquo(response), x)
+
+    explanatory_cols <- tidyselect::eval_select(rlang::enquo(explanatory), x)
+
     x <-
       dplyr::mutate(x, across(c({{response}}, {{explanatory}}),
                               ~labelled::remove_val_labels(.x)))
-    if(
-      stat %in% c("means",
-                  "diff in medians", "diff in props", "Chisq", "ratio of props")) {
-      x <-
-        dplyr::mutate(x, across({{explanatory}}, ~as.factor(.x)))
-    }
 
-    out <-
-    names(response_cols)
-    out <- rlang::set_names(out)
-    purrr::map_dfr(out, .id = "outcome", .f = function(outcome) {
-      out <- names(explanatory_cols)
-      out <- rlang::set_names(out)
-      purrr::map_dfr(out, .id = "predictor", .f = function(predictor) {
-        out <-
-            t.test(x[[outcome]], x[[predictor]])
-              broom::tidy(out)
+    resp_cols <- names(response_cols)
+    resp_cols <- rlang::set_names(resp_cols)
+    expl_cols <- names(explanatory_cols)
+    expl_cols <- rlang::set_names(expl_cols)
+
+    purrr::map_dfr(.x = resp_cols,
+                   .id = "response",
+                   .f = function(response) {
+
+                     resp_col <- rlang::sym(response)
+
+                     purrr::map_dfr(.x = expl_cols,
+                     .id = "explanatory",
+                     .f = function(explanatory) {
+
+                       expl_col <- rlang::sym(explanatory)
+                       # x <-
+                       #   tidyr::drop_na(x, response_cols, explanatory_cols)
+
+        infer::t_test(x = x,
+                      response = !!resp_col,
+                      explanatory = !!expl_col,
+                      order = order,
+                      alternative = alternative,
+                      mu = mu,
+                      conf_int = conf_int,
+                      conf_level = conf_level)
           })
       })
   }
--- tests/testthat/test-observe_multiple_comb.R
+++ tests/testthat/test-observe_multiple_comb.R
@@ -1,9 +1,16 @@
-testthat::test_that("observe_multiple_comb", {
+testthat::test_that("t_test_multiple_comb", {
   library(dplyr)
   x <-
-  observe_multiple_comb(x = mtcars,
-                        response = matches("mpg|disp|drat|hp|qsec"),
-                        explanatory = matches("vs|am"), stat = "diff in means") %>%
-    select(outcome, predictor, estimate1, estimate2, p.value)
-  testthat::expect_equal(dim(x), c(10, 5))
+    t_test_multiple_comb(x = mtcars,
+                        response = matches("mpg|disp"),
+                        explanatory = matches("vs"),
+                        # null = "point",
+                        mu = 0,
+                        # stat = "diff in means",
+                        order = c("1", "0"),
+                        alternative = "two-sided",
+                        conf_level = .95#,
+                        # ci_type = "se"
+                        )
+  testthat::expect_equal(dim(x), c(2, 9))
 })
