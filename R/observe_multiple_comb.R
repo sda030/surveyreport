feff7421b2cdad74fa74b9b840a33576aab697b5
Stephan Daus <stephan.daus@gmail.com>
2022-09-24 09:00:58 +0200

    0.0.1.9004

--- R/observe_multiple_comb.R
+++ R/observe_multiple_comb.R
@@ -2,6 +2,7 @@
 #' Extention to the infer package for multiple testing
 #'
 #' @inheritParams infer::t_test
+#' @param test String indicating type of test done for all combinations.
 #'
 #' @importFrom dplyr mutate across
 #' @importFrom tidyselect eval_select all_of
@@ -9,7 +10,7 @@
 #' @importFrom labelled remove_val_labels
 #' @importFrom broom tidy
 #' @importFrom purrr map_dfr
-#' @importFrom rlang set_names enquo
+#' @importFrom rlang set_names enquo arg_match
 #' @importFrom infer t_test
 #' @return A tibble containing for each response-explanatory row combination
 #' columns summarizing the test statistic as obtained from broom::tidy().
@@ -28,10 +29,16 @@ t_test_multiple_comb <-
            order = NULL,
            alternative = "two-sided",
            mu = 0,
+           correct = TRUE,
+           p = NULL,
            conf_int = TRUE,
-           conf_level = 0.95
+           conf_level = 0.95,
+           test = c("t-test two-sample", "chi-square", "prop"),
+           success = NULL,
+           z = FALSE
   ) {
 
+    test <- rlang::arg_match(test, multiple = FALSE)
     response_cols <- tidyselect::eval_select(rlang::enquo(response), x)
 
     explanatory_cols <- tidyselect::eval_select(rlang::enquo(explanatory), x)
@@ -56,17 +63,37 @@ t_test_multiple_comb <-
                      .f = function(explanatory) {
 
                        expl_col <- rlang::sym(explanatory)
-                       # x <-
-                       #   tidyr::drop_na(x, response_cols, explanatory_cols)
 
-        infer::t_test(x = x,
-                      response = !!resp_col,
-                      explanatory = !!expl_col,
-                      order = order,
-                      alternative = alternative,
-                      mu = mu,
-                      conf_int = conf_int,
-                      conf_level = conf_level)
+                       if(test == "t-test two-sample") {
+                         infer::t_test(x = x,
+                                       response = !!resp_col,
+                                       explanatory = !!expl_col,
+                                       order = order,
+                                       alternative = alternative,
+                                       mu = mu,
+                                       conf_int = conf_int,
+                                       conf_level = conf_level)
+                       } else if(test == "chi-square") {
+                         infer::chisq_test(x = x,
+                                       response = !!resp_col,
+                                       explanatory = !!expl_col,
+                                       correct = correct,
+                                       p = p)
+                       } else if(test == "prop") {
+                         infer::prop_test(x = x,
+                                          response = !!resp_col,
+                                          explanatory = !!expl_col,
+                                          order = order,
+                                          alternative = alternative,
+                                          p = p,
+                                          success = success,
+                                          correct = correct,
+                                          z = z,
+                                          conf_int = conf_int,
+                                          conf_level = conf_level)
+                       }
+
+
           })
       })
   }
--- tests/testthat/test-observe_multiple_comb.R
+++ tests/testthat/test-observe_multiple_comb.R
@@ -1,16 +1,33 @@
 testthat::test_that("t_test_multiple_comb", {
   library(dplyr)
   x <-
-    t_test_multiple_comb(x = mtcars,
+    mtcars %>%
+    # mutate(carb = as.factor(carb)) %>%
+    t_test_multiple_comb(x = ,
                         response = matches("mpg|disp"),
                         explanatory = matches("vs"),
-                        # null = "point",
                         mu = 0,
-                        # stat = "diff in means",
                         order = c("1", "0"),
                         alternative = "two-sided",
-                        conf_level = .95#,
-                        # ci_type = "se"
+                        conf_level = .95,
+                        test = "t-test two-sample"
                         )
   testthat::expect_equal(dim(x), c(2, 9))
+})
+
+testthat::test_that("t_test_multiple_comb", {
+  library(dplyr)
+  x <-
+    mtcars %>%
+    mutate(across(matches("carb|am|cyl"), ~as.factor(.x))) %>%
+    t_test_multiple_comb(x = ,
+                         response = matches("carb|am"),
+                         explanatory = matches("cyl"),
+                         mu = 0,
+                         order = c("1", "0"),
+                         alternative = "two-sided",
+                         conf_level = .95,
+                         test = "chi-square"
+    )
+  testthat::expect_equal(dim(x), c(2, 5))
 })
